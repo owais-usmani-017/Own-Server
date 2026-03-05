cef5039a3e42cd5b34cd0bbde8cdd352bd187fd8
owaisusmani-436 <owaisusmani556677@gmail.com>
2026-03-05 17:06:51 +0530

    implement request handling and MIME type mapping in server

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const path = require("path");
 
 const port = 8087;
 
-const server = http.createServer();
+const server = http.createServer((req,res) =>{
+    const filePath = path.join(__dirname , req.url ==='/' ? "index.html" : "req.url");
+
+    const extName = string(path.extname(filePath).toLowerCase());
+
+    const mimeTypes = {
+        '.html' : 'text/html',
+        '.css' : 'text/css',
+        '.js' : 'text/javascript',
+        '.png' : 'text/png',
+    }
+});
 
 server.listen(port , () => {
     console.log("server is running on port " + port)
